27a654a8d118e74cbbe2100a040d9a4d9660a315
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-04-22 15:31:28 -0700

    Text change on custom 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -7,9 +7,9 @@ import { responsiveTitle1 } from '../components/typography.module.css'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title='404: Not found' />
+    <SEO title="404: Not found" />
     <Container>
-      <h1 className={responsiveTitle1}>Not found</h1>
+      <h1 className={responsiveTitle1}>Was not found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Container>
   </Layout>
